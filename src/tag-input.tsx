761977e73d5f923f2335bee51abd3462a024c8ea
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-24 09:24:58 +0200

    fix: 🐛 correct packaging

--- src/tag-input.tsx
+++ src/tag-input.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { NavFrontendInputProps } from 'nav-frontend-skjema';
 import 'nav-frontend-skjema-style';
